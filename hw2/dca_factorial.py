68ad981f7d2b447cb3ce3182f38f1a3ee9790fe2
abhay <abhay2001sri@gmail.com>
2026-02-07 23:20:23 -0500

    chore: divide and conquer factorial complete

--- hw2/dca_factorial.py
+++ hw2/dca_factorial.py
@@ -13,5 +13,10 @@ def dca_fact(p, q):
 
 
 if __name__ == "__main__":
-    n = int(input())
-    print(f"{n}! =", dca_fact(1, n))
+    print(f"{0}! = {dca_fact(1, 0)}")
+    print(f"{1}! = {dca_fact(1, 1)}")
+    print(f"{2}! = {dca_fact(1, 2)}")
+    print(f"{3}! = {dca_fact(1, 3)}")
+    print(f"{5}! = {dca_fact(1, 5)}")
+    print(f"{8}! = {dca_fact(1, 8)}")
+    print(f"{10}! = {dca_fact(1, 10)}")
